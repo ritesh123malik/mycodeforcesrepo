d0aecffbed93d56ea37478e548c564209b23995a
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-05-29 17:04:46 +0530

    Codeforces: Sync solution for [1A] Theatre Square

---
+++ codeforces/1A-theatre-square/Solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	long long a;
+	long long b;
+	cin>>a;
+	cin>>b;
+	long long c;
+	cin>>c;
+	long long x=(a+c-1)/c;
+	long long y=(b+c-1)/c;
+	cout<<x*y;
+	
+	
+ 
+}
