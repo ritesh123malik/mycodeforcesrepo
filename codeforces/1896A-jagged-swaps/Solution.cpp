6d02c86451cbababe894003227abc4a47269a27e
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-05-29 17:03:24 +0530

    Codeforces: Sync solution for [1896A] Jagged Swaps

---
+++ codeforces/1896A-jagged-swaps/Solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        int i;
+        for(i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        if(arr[0]==1){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
