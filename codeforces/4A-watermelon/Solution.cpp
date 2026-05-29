93f89032049529850ca5f59481cad48c2b846fe7
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-05-29 16:56:42 +0530

    Codeforces: Sync solution for [4A] Watermelon

---
+++ codeforces/4A-watermelon/Solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    if(t%2==0&&t!=2){
+        cout<<"YES";
+    }
+    else if(t==2){
+        cout<<"NO\n";
+    } 
+    else{
+        cout<<"NO";
+    }
+ 
+}
