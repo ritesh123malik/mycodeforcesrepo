eb769b085e9088ce44e4667f6b360d45366e76c4
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-06-09 02:02:06 +0530

    Codeforces: Sync solution for [1611B] Team Composition: Programmers and Mathematicians

---
+++ codeforces/1611B-team-composition-programmers-and-mathematicians/Solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    long long t;
+    cin>>t;
+ 
+    while(t--){
+ 
+        long long a,b;
+        cin>>a>>b;
+ 
+        cout<<min(min(a,b),(a+b)/4)<<endl;
+    }
+}
