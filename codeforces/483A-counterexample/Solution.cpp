878a3d618a4e904ebaa429d58ab18d5ba173860d
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-06-15 07:11:36 +0530

    Codeforces: Sync solution for [483A] Counterexample

---
+++ codeforces/483A-counterexample/Solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long l,r;
+    cin>>l>>r;
+ 
+    if(r-l<2){
+        cout<<-1<<endl;
+        return 0;
+    }
+ 
+    if(l%2==0){
+        cout<<l<<" "<<l+1<<" "<<l+2<<endl;
+    }
+    else{
+        if(l+3<=r){
+            cout<<l+1<<" "<<l+2<<" "<<l+3<<endl;
+        }
+        else{
+            cout<<-1<<endl;
+        }
+    }
+ 
+    return 0;
+}
